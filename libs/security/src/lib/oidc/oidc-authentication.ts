d3afa5a4f4c63ba96bc130d6ca480a94e524952e
andreasernst <andreas.ernst@contabo.de>
2026-05-13 10:58:52 +0200

    security

--- libs/security/src/lib/oidc/oidc-authentication.ts
+++ libs/security/src/lib/oidc/oidc-authentication.ts
@@ -1,17 +1,16 @@
 
 import { OIDCUser } from "./oidc-user";
 
-import { Authentication, AuthenticationRequest } from '../authentication';
+import { Authentication } from '../authentication';
 import { OIDCTicket } from "./oidc-session-manager";
 import { Session } from "../session.interface";
 
-export abstract class OIDCAuthentication
-  implements Authentication<any, OIDCUser, OIDCTicket>
-{
+export abstract class OIDCAuthentication implements Authentication<any, OIDCUser, OIDCTicket> {
+  // implement Authentication
+
   abstract login(request: any): Promise<Session<OIDCUser, OIDCTicket>>;
 
   abstract logout(): Promise<void>;
 
   abstract start(): Promise<Session<OIDCUser, OIDCTicket> | null>;
-  // implement Authentication
 }
--- libs/security/src/lib/oidc/oidc-session-manager.ts
+++ libs/security/src/lib/oidc/oidc-session-manager.ts
@@ -1,6 +1,4 @@
-import { Session } from '../session.interface';
 import { OIDCUser } from './oidc-user';
-import { Authentication } from '../authentication';
 import { SessionManager } from '../session-manager';
 
 export interface OIDCTicket {
@@ -8,68 +6,5 @@ export interface OIDCTicket {
   refreshToken?: string;
   idToken?: string;
 }
-/*
-
-export class OIDCAuthentication implements Authentication<any, OIDCUser, OIDCTicket> {
-  // instance data
-
-  private keycloak: KeycloakInstance;
-
-  // constructor
-
-  constructor(config: { url: string; realm: string; clientId: string }) {
-    this.keycloak = new (Keycloak as any)(config);
-  }
-
-  // implement
-
-  async start(): Promise<Session<OIDCUser, OIDCTicket> | null> {
-    await this.keycloak.init({
-      onLoad: 'check-sso',
-      flow: 'standard',
-      checkLoginIframe: false,
-    });
-
-    if (!this.keycloak.authenticated) {
-      return null;
-    }
-
-    return this.buildSession();
-  }
-
-  async login(): Promise<Session<OIDCUser, OIDCTicket>> {
-    // Redirect to login
-    await this.keycloak.login({
-      redirectUri: window.location.href,
-    });
-
-    // In OIDC standard flow this line is never reached
-    throw new Error('Redirecting for OIDC login');
-  }
-
-  async logout(): Promise<void> {
-    await this.keycloak.logout({
-      redirectUri: window.location.origin,
-    });
-  }
-
-  // private
-
-  private buildSession(): Session<OIDCUser, OIDCTicket> {
-    return {
-      user: this.keycloak.idTokenParsed as OIDCUser,
-      ticket: {
-        accessToken: this.keycloak.token!,
-        refreshToken: this.keycloak.refreshToken,
-        idToken: this.keycloak.idToken,
-      },
-      expiry: this.keycloak.tokenParsed?.exp
-        ? this.keycloak.tokenParsed.exp * 1000
-        : undefined,
-      sessionLocals: {},
-    };
-  }
-}
 
 export type OIDCSessionManager = SessionManager<OIDCUser, OIDCTicket>;
-*/
