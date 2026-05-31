4b57413210aba4b5e9c0a52f1336d390d85095bd
Andreas Ernst <andreas.ernst7@gmail.com>
2026-05-31 11:13:12 +0200

    new tool

--- tools/xmi-compiler/src/naming/strategies/snake-case.ts
+++
@@ -1,9 +0,0 @@
-import type { NamingTransform } from '../strategy.js'
-import { toSnakeCase } from '../../utils/string-utils.js'
-
-/** Converts camelCase / PascalCase identifiers to lower_snake_case. */
-export class SnakeCaseTransform implements NamingTransform {
-  apply(name: string): string {
-    return toSnakeCase(name)
-  }
-}
